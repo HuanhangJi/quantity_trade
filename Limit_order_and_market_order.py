6d59e8f92a935119854474f0234e6b365ff6f0a5
Sansi <jihuanhangla@gmail.com>
2022-04-02 13:13:12 +0800

    Create Limit_order_and_market_order.py

---
+++ Limit_order_and_market_order.py
@@ -0,0 +1,17 @@
+class online_trade:
+    def __init__(self, bid, ask):
+        self.bid = bid
+        self.ask = ask
+
+    def sell_limit(self, price, lots):
+        pass
+
+    def buy_limit(self, price, lots):
+        pass
+
+    def sell_market(self, lots):
+        pass
+
+    def buy_market(self, lots):
+        pass
+
